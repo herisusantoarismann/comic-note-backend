f138f499f8fc74b6e818e6e358a27947e3b75a87
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-04-18 11:49:04 +0700

    (+) api authentication

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('auth')
+export class UserController {
+  constructor(private readonly usersService: UserService) {}
+
+  @Post('register')
+  @HttpCode(201)
+  async register(@Body() createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+    const user = await this.usersService.register(name, email, password);
+    return {
+      success: true,
+      data: {
+        ...user,
+      },
+    };
+  }
+
+  @Post('login')
+  async login(@Body() LoginDto: { email: string; password: string }) {
+    const { email, password } = LoginDto;
+    const user = await this.usersService.login(email, password);
+    if (user) {
+      return { user };
+    } else {
+      return {
+        success: false,
+        message: 'Invalid email or password',
+      };
+    }
+  }
+}
