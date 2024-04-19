a0d3c4481b3617e0763f2f60a5490e8c048ab033
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-04-19 14:19:53 +0700

    initial api notifications

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+// src/notification/notification.controller.ts
 
-@Controller('notification')
-export class NotificationController {}
+import { Controller, Get, Patch, Param, Body, Req } from '@nestjs/common';
+import { NotificationService } from './notification.service';
+import { Notification } from '@prisma/client'; // Sesuaikan dengan struktur model Prisma Anda
+
+@Controller('notifications')
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  // Endpoint untuk mendapatkan semua notifikasi
+  @Get()
+  async getAllNotifications(@Req() request: any): Promise<Notification[]> {
+    const user = request?.user;
+
+    return this.notificationService.findAll(+user.id);
+  }
+
+  // Endpoint untuk membaca notifikasi individual berdasarkan ID
+  @Patch(':id/read')
+  async markNotificationAsRead(
+    @Req() request: any,
+    @Param('id') id: number,
+  ): Promise<Notification> {
+    const user = request?.user;
+
+    return this.notificationService.markAsRead(+user.id, id);
+  }
+
+  // Endpoint untuk membaca semua notifikasi
+  @Patch('readAll')
+  async markAllNotificationsAsRead(@Req() request: any): Promise<void> {
+    const user = request.user;
+
+    await this.notificationService.markAllAsRead(user.id);
+  }
+}
--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -1,4 +1,80 @@
+// src/notification/notification.service.ts
+
 import { Injectable } from '@nestjs/common';
+import { Notification } from '@prisma/client';
+import * as schedule from 'node-schedule';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(private prisma: PrismaService) {
+    this.scheduleNotificationCheck();
+  }
+
+  async scheduleNotificationCheck(): Promise<void> {
+    // Atur pengecekan notifikasi setiap jam 6 pagi
+    schedule.scheduleJob('* 0 6 * * *', async () => {
+      await this.checkAndUpdateNotifications();
+    });
+  }
+
+  async checkAndUpdateNotifications(): Promise<void> {
+    // Dapatkan daftar komik yang diperbarui pada hari ini
+    const updatedComics = await this.prisma.getPrisma().comic.findMany({
+      where: { updateDay: new Date().getDay() },
+    });
+
+    // Dapatkan daftar pengguna yang menyukai komik yang diperbarui
+    for (const comic of updatedComics) {
+      const users = await this.prisma.getPrisma().user.findMany({
+        where: { comic: { some: { id: comic.id } } },
+      });
+
+      // Kirim notifikasi ke setiap pengguna yang menyukai komik yang diperbarui
+      for (const user of users) {
+        await this.createNotification(
+          user.id,
+          'New Comic Update',
+          `A new update for ${comic.title} is available!`,
+        );
+      }
+    }
+  }
+
+  async findAll(userId: number): Promise<Notification[]> {
+    return this.prisma.getPrisma().notification.findMany({
+      where: { userId },
+    });
+  }
+
+  async createNotification(
+    userId: number,
+    title: string,
+    body: string,
+  ): Promise<Notification> {
+    return this.prisma.getPrisma().notification.create({
+      data: {
+        title,
+        body,
+        user: { connect: { id: userId } }, // Menghubungkan notifikasi dengan pengguna yang bersangkutan
+      },
+    });
+  }
+
+  async markAsRead(
+    notificationId: number,
+    userId: number,
+  ): Promise<Notification> {
+    return this.prisma.getPrisma().notification.update({
+      where: { id: notificationId, userId },
+      data: { read: true },
+    });
+  }
+
+  async markAllAsRead(userId: number): Promise<void> {
+    await this.prisma.getPrisma().notification.updateMany({
+      data: { read: true },
+      where: { userId, read: false },
+    });
+  }
+}
